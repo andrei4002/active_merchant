5e6491db7a9dbd500b39ee3311a79ac583c24285
andrei <andrei@andrei-M4600.(none)>
2011-11-29 12:27:48 +0200

    added recurring payments

--- lib/active_merchant/billing/gateways/paypal.rb
+++ lib/active_merchant/billing/gateways/paypal.rb
@@ -1,5 +1,6 @@
 require File.dirname(__FILE__) + '/paypal/paypal_common_api'
 require File.dirname(__FILE__) + '/paypal_express'
+require File.dirname(__FILE__) + '/paypal/paypal_recurring_payments'
 
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
